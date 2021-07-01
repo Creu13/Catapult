3513ac7f2becb23158fff25f5046c289f66d9f0e
Creu13 <77082590+Creu13@users.noreply.github.com>
2021-06-30 17:54:33 -0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,53 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push()
+            stroke("#54270F");
+            if(pointA > 240) {
+                strokeWeight(7);
+                line(pointA.x - 25, pointA.y, pointB.x - 10, pointB.y);
+                line(pointA.x - 25, pointA.y, pointB.x + 30, pointB.y - 3);
+                /* + 30 > is the sling2 pic ling of slingshot
+                -3 > making the points move up a bit
+                -10 > how far back the sling1 goes back
+                */
+                image(this.sling3,pointA.x - 30, pointA.y -10,15,30);
+            }
+            else {
+                strokeWeight(3);
+            line(pointA.x - 25, pointA.y, pointB.x - 10, pointB.y);
+            line(pointA.x - 25, pointA.y, pointB.x + 30, pointB.y - 3);
+            /* + 30 > is the sling2 pic ling of slingshot
+            -3 > making the points move up a bit
+            -10 > how far back the sling1 goes back
+            */
+            image(this.sling3,pointA.x - 30, pointA.y - 10,15,30);
+            }
+            pop()
+        }
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
+    }
+    
+}
